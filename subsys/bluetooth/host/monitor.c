de0d9a0243e440eecfa4ba6f607e572072440913
Dominik Kilian <Dominik.Kilian@nordicsemi.no>
2023-02-15 14:41:33 +0100

    Draft of HCI monitor for live Wireshark capture

--- subsys/bluetooth/host/monitor.c
+++ subsys/bluetooth/host/monitor.c
@@ -27,370 +27,297 @@
 
 #include "monitor.h"
 
+#if !defined(CONFIG_BT_MONITOR_LOG_LEVEL)
+#define CONFIG_BT_MONITOR_LOG_LEVEL 2
+#endif
+
+#define LOG_LEVEL CONFIG_BT_MONITOR_LOG_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(bt_monitor);
+
+#define HCI_H4_NONE 0x00
+#define HCI_H4_CMD  0x01
+#define HCI_H4_ACL  0x02
+#define HCI_H4_SCO  0x03
+#define HCI_H4_EVT  0x04
+#define HCI_H4_ISO  0x05
+
+//#define CONFIG_HCI_PACKETS_ARE_H4 0
+#define CONFIG_HCI_MONITOR_H4_WITH_PHDR 1
+#define CONFIG_HCI_MONITOR_NO_HEADER 1
+#define CONFIG_HCI_MONITOR_WAIT_FOR_RTT 1
+
 /* This is the same default priority as for other console handlers,
  * except that we're not exporting it as a Kconfig variable until a
  * clear need arises.
  */
 #define MONITOR_INIT_PRIORITY 60
 
-/* These defines follow the values used by syslog(2) */
-#define BT_LOG_ERR      3
-#define BT_LOG_WARN     4
-#define BT_LOG_INFO     6
-#define BT_LOG_DBG      7
-
-/* TS resolution is 1/10th of a millisecond */
-#define MONITOR_TS_FREQ 10000
-
-/* Maximum (string) length of a log message */
-#define MONITOR_MSG_MAX 128
-
-enum {
-	BT_LOG_BUSY,
-	BT_CONSOLE_BUSY,
-};
-
-static atomic_t flags;
-
-static struct {
-	atomic_t cmd;
-	atomic_t evt;
-	atomic_t acl_tx;
-	atomic_t acl_rx;
-#if defined(CONFIG_BT_BREDR)
-	atomic_t sco_tx;
-	atomic_t sco_rx;
-#endif
-	atomic_t other;
-} drops;
-
-static void drop_add(uint16_t opcode)
-{
-	switch (opcode) {
-	case BT_MONITOR_COMMAND_PKT:
-		atomic_inc(&drops.cmd);
-		break;
-	case BT_MONITOR_EVENT_PKT:
-		atomic_inc(&drops.evt);
-		break;
-	case BT_MONITOR_ACL_TX_PKT:
-		atomic_inc(&drops.acl_tx);
-		break;
-	case BT_MONITOR_ACL_RX_PKT:
-		atomic_inc(&drops.acl_rx);
-		break;
-#if defined(CONFIG_BT_BREDR)
-	case BT_MONITOR_SCO_TX_PKT:
-		atomic_inc(&drops.sco_tx);
-		break;
-	case BT_MONITOR_SCO_RX_PKT:
-		atomic_inc(&drops.sco_rx);
-		break;
-#endif
-	default:
-		atomic_inc(&drops.other);
-		break;
-	}
-}
-
-#if defined(CONFIG_BT_DEBUG_MONITOR_RTT)
 #include <SEGGER_RTT.h>
 
 #define RTT_BUFFER_NAME CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER_NAME
 #define RTT_BUF_SIZE CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER_SIZE
 
-static void monitor_send(const void *data, size_t len)
-{
-	static uint8_t rtt_buf[RTT_BUF_SIZE];
-	static size_t rtt_buf_offset;
-	struct bt_monitor_hdr *hdr;
-	unsigned int cnt = 0;
-	bool drop;
+/* https://www.tcpdump.org/linktypes.html */
+#define DLT_BLUETOOTH_HCI_H4 187
+#define DLT_BLUETOOTH_HCI_H4_WITH_PHDR 201
 
-	/* Drop any packet which cannot fit the buffer */
-	drop = rtt_buf_offset + len > sizeof(rtt_buf);
-	if (!drop) {
-		(void)memcpy(rtt_buf + rtt_buf_offset, data, len);
-	}
+#define PHDR_FROM_CONTROLLER_TO_HOST 0x00
+#define PHDR_FROM_HOST_TO_CONTROLLER 0x01
 
-	rtt_buf_offset += len;
+/*
+https://wiki.wireshark.org/Bluetooth
+https://wiki.wireshark.org/Development/LibpcapFileFormat
+*/
 
-	/* Check if the packet is complete */
-	hdr = (struct bt_monitor_hdr *)rtt_buf;
-	if (rtt_buf_offset < sizeof(hdr->data_len) + hdr->data_len) {
-		return;
-	}
+static const struct {
+	uint32_t magic_number;   /* magic number */
+	uint16_t version_major;  /* major version number */
+	uint16_t version_minor;  /* minor version number */
+	int32_t  thiszone;       /* GMT to local correction */
+	uint32_t sigfigs;        /* accuracy of timestamps */
+	uint32_t snaplen;        /* max length of captured packets, in octets */
+	uint32_t network;        /* data link type */
+} file_header = {
+	0xa1b2c3d4,
+	2,
+	4,
+	0,
+	0,
+	65536,
+	IS_ENABLED(CONFIG_HCI_MONITOR_H4_WITH_PHDR) ?
+		DLT_BLUETOOTH_HCI_H4_WITH_PHDR : DLT_BLUETOOTH_HCI_H4,
+};
 
-	if (!drop) {
-		SEGGER_RTT_LOCK();
-		cnt = SEGGER_RTT_WriteNoLock(CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER,
-					     rtt_buf, rtt_buf_offset);
-		SEGGER_RTT_UNLOCK();
-	}
-
-	if (!cnt) {
-		drop_add(hdr->opcode);
-	}
-
-	/* Prepare for the next packet */
-	rtt_buf_offset = 0;
-}
-
-static void poll_out(char c)
-{
-	monitor_send(&c, sizeof(c));
-}
-#elif defined(CONFIG_BT_DEBUG_MONITOR_UART)
-static const struct device *const monitor_dev =
-	DEVICE_DT_GET(DT_CHOSEN(zephyr_bt_mon_uart));
-
-static void poll_out(char c)
-{
-	uart_poll_out(monitor_dev, c);
-}
-
-static void monitor_send(const void *data, size_t len)
-{
-	const uint8_t *buf = data;
-
-	while (len--) {
-		poll_out(*buf++);
-	}
-}
-#endif /* CONFIG_BT_DEBUG_MONITOR_UART */
-
-static void encode_drops(struct bt_monitor_hdr *hdr, uint8_t type,
-			 atomic_t *val)
-{
-	atomic_val_t count;
-
-	count = atomic_set(val, 0);
-	if (count) {
-		hdr->ext[hdr->hdr_len++] = type;
-		hdr->ext[hdr->hdr_len++] = MIN(count, 255);
-	}
-}
-
-static uint32_t monitor_ts_get(void)
-{
-	return (k_cycle_get_32() /
-		(sys_clock_hw_cycles_per_sec() / MONITOR_TS_FREQ));
-}
-
-static inline void encode_hdr(struct bt_monitor_hdr *hdr, uint32_t timestamp,
-			      uint16_t opcode, uint16_t len)
-{
-	struct bt_monitor_ts32 *ts;
-
-	hdr->opcode   = sys_cpu_to_le16(opcode);
-	hdr->flags    = 0U;
-
-	ts = (void *)hdr->ext;
-	ts->type = BT_MONITOR_TS32;
-	ts->ts32 = timestamp;
-	hdr->hdr_len = sizeof(*ts);
-
-	encode_drops(hdr, BT_MONITOR_COMMAND_DROPS, &drops.cmd);
-	encode_drops(hdr, BT_MONITOR_EVENT_DROPS, &drops.evt);
-	encode_drops(hdr, BT_MONITOR_ACL_TX_DROPS, &drops.acl_tx);
-	encode_drops(hdr, BT_MONITOR_ACL_RX_DROPS, &drops.acl_rx);
-#if defined(CONFIG_BT_BREDR)
-	encode_drops(hdr, BT_MONITOR_SCO_TX_DROPS, &drops.sco_tx);
-	encode_drops(hdr, BT_MONITOR_SCO_RX_DROPS, &drops.sco_rx);
+struct RecordHeader {
+	uint32_t ts_sec;         /* timestamp seconds */
+	uint32_t ts_usec;        /* timestamp microseconds */
+	uint32_t incl_len;       /* number of octets of packet saved in file */
+	uint32_t orig_len;       /* actual length of packet */
+#if defined(CONFIG_HCI_MONITOR_H4_WITH_PHDR)
+	uint32_t phdr;           /* direction flag */
 #endif
-	encode_drops(hdr, BT_MONITOR_OTHER_DROPS, &drops.other);
+	uint8_t h4_packet_type[4];
+};
 
-	hdr->data_len = sys_cpu_to_le16(4 + hdr->hdr_len + len);
+uint64_t get_cycles_64() {
+	if (IS_ENABLED(CONFIG_TIMER_HAS_64BIT_CYCLE_COUNTER)) {
+		return k_cycle_get_64();
+	} else {
+		static atomic_t high_atomic;
+		uint32_t high = (uint32_t)atomic_get(&high_atomic);
+		uint32_t t = k_cycle_get_32();
+
+		if ((t >> 30) != (high & 3)) {
+			if ((t >> 30) == ((high + 1) & 3)) {
+				atomic_cas(&high_atomic, high, high + 1);
+				high += 1;
+			} else if ((t >> 30) == ((high + 2) & 3)) {
+				atomic_cas(&high_atomic, high, high + 2);
+				high += 2;
+			} else {
+				high--;
+			}
+		}
+
+		return ((uint64_t)high << 30) | (uint64_t)t;
+	}
 }
 
 void bt_monitor_send(uint16_t opcode, const void *data, size_t len)
 {
-	struct bt_monitor_hdr hdr;
+	struct RecordHeader header;
+	unsigned int cnt;
+	size_t incl_len;
+	size_t header_size;
+	uint64_t t = get_cycles_64();
+	uint64_t per_sec = sys_clock_hw_cycles_per_sec();
+	uint32_t sec = (uint32_t)(t / per_sec);
+	uint64_t cycles = t % per_sec;
+	uint32_t us = (uint32_t)((cycles * 1000000) / per_sec);
 
-	if (atomic_test_and_set_bit(&flags, BT_LOG_BUSY)) {
-		drop_add(opcode);
+	uint8_t hci_h4_type;
+	uint32_t phdr = 0;
+
+	switch (opcode) {
+	case BT_MONITOR_COMMAND_PKT:
+		hci_h4_type = HCI_H4_CMD;
+		phdr = PHDR_FROM_CONTROLLER_TO_HOST;
+		break;
+	case BT_MONITOR_EVENT_PKT:
+		hci_h4_type = HCI_H4_EVT;
+		phdr = PHDR_FROM_HOST_TO_CONTROLLER;
+		break;
+	case BT_MONITOR_ACL_TX_PKT:
+		hci_h4_type = HCI_H4_ACL;
+		phdr = PHDR_FROM_CONTROLLER_TO_HOST;
+		break;
+	case BT_MONITOR_ACL_RX_PKT:
+		hci_h4_type = HCI_H4_ACL;
+		phdr = PHDR_FROM_HOST_TO_CONTROLLER;
+		break;
+	case BT_MONITOR_ISO_TX_PKT:
+		hci_h4_type = HCI_H4_ISO;
+		phdr = PHDR_FROM_CONTROLLER_TO_HOST;
+		break;
+	case BT_MONITOR_ISO_RX_PKT:
+		hci_h4_type = HCI_H4_ISO;
+		phdr = PHDR_FROM_HOST_TO_CONTROLLER;
+		break;
+	default:
 		return;
 	}
 
-	encode_hdr(&hdr, monitor_ts_get(), opcode, len);
+	incl_len = len;
+	header_size = offsetof(struct RecordHeader, h4_packet_type);
 
-	monitor_send(&hdr, BT_MONITOR_BASE_HDR_LEN + hdr.hdr_len);
-	monitor_send(data, len);
+	if (IS_ENABLED(CONFIG_HCI_MONITOR_H4_WITH_PHDR)) {
+		incl_len += 4;
+	}
 
-	atomic_clear_bit(&flags, BT_LOG_BUSY);
+	if (!IS_ENABLED(CONFIG_HCI_PACKETS_ARE_H4)) {
+		incl_len++;
+		header_size++;
+	}
+
+	header.ts_sec = sys_cpu_to_le32(sec);
+	header.ts_usec = sys_cpu_to_le32(us);
+	header.incl_len = sys_cpu_to_le32(incl_len);
+	header.orig_len = sys_cpu_to_le32(incl_len);
+#if defined(CONFIG_HCI_MONITOR_H4_WITH_PHDR)
+	header.phdr = sys_cpu_to_be32(phdr);
+#endif
+	header.h4_packet_type[0] = hci_h4_type;
+
+	SEGGER_RTT_LOCK();
+	cnt = SEGGER_RTT_GetAvailWriteSpace(CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER);
+	if (cnt >= sizeof(header) + len) {
+		SEGGER_RTT_WriteNoLock(CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER,
+							&header, header_size);
+		SEGGER_RTT_WriteNoLock(CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER,
+							data, len);
+	}
+	SEGGER_RTT_UNLOCK();
+
+	if (cnt < sizeof(header) + len) {
+		static int total_lost = 0;
+		const uint8_t *buf = (const uint8_t *)data;
+
+		total_lost++;
+		LOG_WRN("BT Monitor packet dropped, total=%d, size=%d, type=%d, from_host=%d, %02X %02X %02X %02X ...", total_lost, len, opcode, phdr, buf[0], buf[1], buf[2], buf[3]);
+	}
 }
 
 void bt_monitor_new_index(uint8_t type, uint8_t bus, const bt_addr_t *addr,
 			  const char *name)
 {
-	struct bt_monitor_new_index pkt;
-
-	pkt.type = type;
-	pkt.bus = bus;
-	memcpy(pkt.bdaddr, addr, 6);
-	strncpy(pkt.name, name, sizeof(pkt.name) - 1);
-	pkt.name[sizeof(pkt.name) - 1] = '\0';
-
-	bt_monitor_send(BT_MONITOR_NEW_INDEX, &pkt, sizeof(pkt));
+	/* not supported by pcap format */
 }
 
-#if !defined(CONFIG_UART_CONSOLE) && !defined(CONFIG_RTT_CONSOLE) && !defined(CONFIG_LOG_PRINTK)
-static int monitor_console_out(int c)
-{
-	static char buf[MONITOR_MSG_MAX];
-	static size_t len;
-
-	if (atomic_test_and_set_bit(&flags, BT_CONSOLE_BUSY)) {
-		return c;
-	}
-
-	if (c != '\n' && len < sizeof(buf) - 1) {
-		buf[len++] = c;
-		atomic_clear_bit(&flags, BT_CONSOLE_BUSY);
-		return c;
-	}
-
-	buf[len++] = '\0';
-
-	bt_monitor_send(BT_MONITOR_SYSTEM_NOTE, buf, len);
-	len = 0;
-
-	atomic_clear_bit(&flags, BT_CONSOLE_BUSY);
-
-	return c;
-}
-
-extern void __printk_hook_install(int (*fn)(int));
-extern void __stdout_hook_install(int (*fn)(int));
-#endif /* !CONFIG_UART_CONSOLE && !CONFIG_RTT_CONSOLE && !CONFIG_LOG_PRINTK */
-
-#ifndef CONFIG_LOG_MODE_MINIMAL
-struct monitor_log_ctx {
-	size_t total_len;
-	char msg[MONITOR_MSG_MAX];
+static const struct RecordHeader dummy_reset_record = {
+	.ts_sec = 0,
+	.ts_usec = 0,
+#if defined(CONFIG_HCI_MONITOR_H4_WITH_PHDR)
+	.incl_len = 8,
+	.orig_len = 8,
+	.phdr = 0,
+#else
+	.incl_len = 4,
+	.orig_len = 4,
+#endif
+	.h4_packet_type = { 0x01, 0x03, 0x0c, 0x00 },
 };
-
-static int monitor_log_out(uint8_t *data, size_t length, void *user_data)
-{
-	struct monitor_log_ctx *ctx = user_data;
-	size_t i;
-
-	for (i = 0; i < length && ctx->total_len < sizeof(ctx->msg); i++) {
-		/* With CONFIG_LOG_PRINTK the line terminator will come as
-		 * as part of messages.
-		 */
-		if (IS_ENABLED(CONFIG_LOG_PRINTK) &&
-		    (data[i] == '\r' || data[i] == '\n')) {
-			break;
-		}
-
-		ctx->msg[ctx->total_len++] = data[i];
-	}
-
-	return length;
-}
-
-static uint8_t buf;
-
-LOG_OUTPUT_DEFINE(monitor_log_output, monitor_log_out, &buf, 1);
-
-static inline uint8_t monitor_priority_get(uint8_t log_level)
-{
-	static const uint8_t prios[] = {
-		[LOG_LEVEL_NONE]  = 0,
-		[LOG_LEVEL_ERR]   = BT_LOG_ERR,
-		[LOG_LEVEL_WRN]   = BT_LOG_WARN,
-		[LOG_LEVEL_INF]   = BT_LOG_INFO,
-		[LOG_LEVEL_DBG]   = BT_LOG_DBG,
-	};
-
-	if (log_level < ARRAY_SIZE(prios)) {
-		return prios[log_level];
-	}
-
-	return BT_LOG_DBG;
-}
-
-static void monitor_log_process(const struct log_backend *const backend,
-				union log_msg_generic *msg)
-{
-	struct bt_monitor_user_logging user_log;
-	struct monitor_log_ctx ctx;
-	struct bt_monitor_hdr hdr;
-	static const char id[] = "bt";
-
-	log_output_ctx_set(&monitor_log_output, &ctx);
-
-	ctx.total_len = 0;
-	log_output_msg_process(&monitor_log_output, &msg->log,
-			       LOG_OUTPUT_FLAG_CRLF_NONE);
-
-	if (atomic_test_and_set_bit(&flags, BT_LOG_BUSY)) {
-		drop_add(BT_MONITOR_USER_LOGGING);
-		return;
-	}
-
-	encode_hdr(&hdr, (uint32_t)log_msg_get_timestamp(&msg->log),
-		   BT_MONITOR_USER_LOGGING,
-		   sizeof(user_log) + sizeof(id) + ctx.total_len + 1);
-
-	user_log.priority = monitor_priority_get(log_msg_get_level(&msg->log));
-	user_log.ident_len = sizeof(id);
-
-	monitor_send(&hdr, BT_MONITOR_BASE_HDR_LEN + hdr.hdr_len);
-	monitor_send(&user_log, sizeof(user_log));
-	monitor_send(id, sizeof(id));
-	monitor_send(ctx.msg, ctx.total_len);
-
-	/* Terminate the string with null */
-	poll_out('\0');
-
-	atomic_clear_bit(&flags, BT_LOG_BUSY);
-}
-
-static void monitor_log_panic(const struct log_backend *const backend)
-{
-}
-
-static void monitor_log_init(const struct log_backend *const backend)
-{
-	log_set_timestamp_func(monitor_ts_get, MONITOR_TS_FREQ);
-}
-
-static const struct log_backend_api monitor_log_api = {
-	.process = monitor_log_process,
-	.panic = monitor_log_panic,
-	.init = monitor_log_init,
-};
-
-LOG_BACKEND_DEFINE(bt_monitor, monitor_log_api, true);
-#endif /* CONFIG_LOG_MODE_MINIMAL */
 
 static int bt_monitor_init(const struct device *d)
 {
 	ARG_UNUSED(d);
-
-#if defined(CONFIG_BT_DEBUG_MONITOR_RTT)
 	static uint8_t rtt_up_buf[RTT_BUF_SIZE];
-
 	SEGGER_RTT_ConfigUpBuffer(CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER,
 				  RTT_BUFFER_NAME, rtt_up_buf, RTT_BUF_SIZE,
 				  SEGGER_RTT_MODE_NO_BLOCK_SKIP);
-#elif defined(CONFIG_BT_DEBUG_MONITOR_UART)
-	__ASSERT_NO_MSG(device_is_ready(monitor_dev));
+	if (!IS_ENABLED(CONFIG_HCI_MONITOR_NO_HEADER)) {
+		SEGGER_RTT_WriteNoLock(CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER,
+							&file_header, sizeof(file_header));
+	} else if (IS_ENABLED(CONFIG_HCI_MONITOR_WAIT_FOR_RTT)) {
+		SEGGER_RTT_WriteNoLock(CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER,
+							&dummy_reset_record,
+							sizeof(dummy_reset_record));
+	}
 
-#if defined(CONFIG_UART_INTERRUPT_DRIVEN)
-	uart_irq_rx_disable(monitor_dev);
-	uart_irq_tx_disable(monitor_dev);
-#endif /* CONFIG_UART_INTERRUPT_DRIVEN */
-#endif /* CONFIG_BT_DEBUG_MONITOR_UART */
-
-#if !defined(CONFIG_UART_CONSOLE) && !defined(CONFIG_RTT_CONSOLE) && !defined(CONFIG_LOG_PRINTK)
-	__printk_hook_install(monitor_console_out);
-	__stdout_hook_install(monitor_console_out);
-#endif /* !CONFIG_UART_CONSOLE && !CONFIG_RTT_CONSOLE && !CONFIG_LOG_PRINTK */
-
+	if (IS_ENABLED(CONFIG_HCI_MONITOR_WAIT_FOR_RTT)) {
+		while (SEGGER_RTT_GetBytesInBuffer(CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER)) {
+			/* Empty loop waiting until JLink consumes the first packet. */
+		}
+	}
 	return 0;
 }
 
 SYS_INIT(bt_monitor_init, PRE_KERNEL_1, MONITOR_INIT_PRIORITY);
+
+/*
+
+Use case 1: Capture to a file (single run - resetting your device will end capturing).
+
+1. Disable CONFIG_HCI_MONITOR_NO_HEADER
+2. Flash and start your application.
+3. Use JLinkRTTLogger to capture packets to a file, e.g.:
+   JLinkRTTLogger -device NRF52840_XXAA -if swd -speed 4000 -rttchannel 1 -usb 683149425 hci.pcap
+4. Press Ctrl-C to exit JLinkRTTLogger
+5. Open your file in Wireshark
+
+Use case 2: Capture to a file (multiple runs - resetting your device will not end capturing).
+
+1. Prepare header file:
+   echo d4c3b2a102000400000000000000000000000100c9000000 | xxd -r -p - header.pcap
+2. Enable CONFIG_HCI_MONITOR_NO_HEADER
+3. Flash and start your application.
+4. Use JLinkRTTLogger to capture packets to a file, e.g.:
+   JLinkRTTLogger -device NRF52840_XXAA -if swd -speed 4000 -rttchannel 1 -usb 683149425 hci.pcap-no-head
+5. Press Ctrl-C to exit JLinkRTTLogger
+7. Add header to your capture:
+   cat header.pcap hci.pcap-no-head > hci.pcap
+6. Open your file in Wireshark
+
+Use case 3: Live capture
+
+1. Prepare header file:
+   echo d4c3b2a102000400000000000000000000000100c9000000 | xxd -r -p - header.pcap
+2. Make two FIFOs:
+   mkfifo /tmp/a
+   mkfifo /tmp/b
+3. Combine header and one FIFO and send them to the second FIFO:
+   cat header.pcap /tmp/a > /tmp/b &
+4. Start listening with Wireshark:
+   wireshark -k -i /tmp/b &
+5. Enable CONFIG_HCI_MONITOR_NO_HEADER
+6. Flash and start your application.
+7. Use JLinkRTTLogger to transfer packets to the FIFO, e.g.:
+   JLinkRTTLogger -device NRF52840_XXAA -if swd -speed 4000 -rttchannel 1 -usb 683149425 /tmp/a
+8. Press Ctrl-C to exit JLinkRTTLoger and stop live capture. Do not use "Stop capturing"
+   button in Wireshark.
+9. You can delete FIFOs if they are not needed anymore or reuse them if you want to repeat
+   the process.
+
+NOTE 1:
+   Enable CONFIG_HCI_MONITOR_WAIT_FOR_RTT if you want to stop you application startup
+   until you connect with RTT. Otherwise, you have to make sure that you are able to connect
+   before RTT buffer overflows, e.g by increasing CONFIG_BT_DEBUG_MONITOR_RTT_BUFFER_SIZE.
+
+NOTE 2:
+   During device reset, the capture output may be corrupted. Probability of that is very small
+   and increases if more data is captured just before the reset. It is because how the RTT works.
+
+NOTE 3:
+   If you enable CONFIG_HCI_MONITOR_WAIT_FOR_RTT and CONFIG_HCI_MONITOR_NO_HEADER, you will see
+   one additional reset packet just after reset at time 0. It is not actual packet that was send
+   by the host, but it is dummy packet needed to detect the RTT connection.
+
+TIP 1:
+   If you want to do multiple live captures, you can:
+   * put "cat" command in a loop. The loop will end when you delete FIFOs:
+     while [ -e /tmp/a ]; do cat header.pcap /tmp/a > /tmp/b ; done &
+   * reuse the same wireshark window by starting again the capture. Wireshark will reuse
+     the same FIFO. Make sure that you started capturing in Wireshark BEFORE starting
+     JLinkRTTLogger again.
+
+*/
